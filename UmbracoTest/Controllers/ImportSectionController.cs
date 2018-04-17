ed42b4d307d198b00fb1125bf672d6a7257f0ad0
Marco Stoker <marco@speak.nl>
2018-04-17 16:43:27 +0200

    import section

---
+++ UmbracoTest/Controllers/ImportSectionController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using umbraco.businesslogic;
+using umbraco.BusinessLogic.Actions;
+using umbraco.interfaces;
+using Umbraco.Web.Models.Trees;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.Trees;
+
+namespace UmbracoTest.Controllers
+{
+    [Application("import", "Import", "icon-files", 15)]
+    public class ImportSectionController : IApplication
+    {
+    }
+
+    [PluginController("import")]
+    [Umbraco.Web.Trees.Tree("import", "importTree", "Import", iconClosed: "icon-doc")]
+    public class ImportSectionTreeController : TreeController
+    {
+        protected override TreeNodeCollection GetTreeNodes(string id, FormDataCollection queryStrings)
+        {
+            var nodes = new TreeNodeCollection();
+            var item = this.CreateTreeNode("dashboard", id, queryStrings, "Import Data", "icon-files", true);
+            if (id == "-1") // root
+            {
+                nodes.Add(item);
+            }
+            return nodes;
+        }
+
+        protected override MenuItemCollection GetMenuForNode(string id, FormDataCollection queryStrings)
+        {
+            var menu = new MenuItemCollection
+            {
+                DefaultMenuAlias = ActionNew.Instance.Alias
+            };
+            menu.Items.Add<ActionNew>("Create");
+            return menu;
+        }
+    }
+}
