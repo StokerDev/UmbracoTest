b5547c680a2f0d8ab39f2d35ed713d89bf5b30b0
Marco Stoker <marcostokerdev@outlook.com>
2018-05-27 16:42:22 +0200

    file post to controller

---
+++ UmbracoTest/Controllers/ImportController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoTest.Controllers
+{
+    public class ImportController : SurfaceController
+    {
+        [HttpPost][ValidateInput(false)]
+        public async Task<JsonResult> ReadXMLFile(string xmlFile)
+        {
+            // Execute a function from the 'ImportService'...
+            return Json("File uploaded successfully!");
+        }
+    }
+}
